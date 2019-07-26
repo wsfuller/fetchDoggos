9f0873f8b6768fb0cc600ff4ea9891b06eb5d2c1
wfuller <wfuller87@gmail.com>
2019-07-26 11:37:38 -0700

    Adding about modal

--- src/components/AboutModal.jsx
+++ src/components/AboutModal.jsx
@@ -3,8 +3,10 @@ import styled from 'styled-components';
 
 import { Header, Image, Menu, Modal } from 'semantic-ui-react';
 
+import Remy from '../assets/images/remy.png';
+
 const StyledLink = styled.a`
-  margin-left: 0.25rem;
+  margin: 0 0.25rem;
 `;
 
 const AboutModal = () => (
@@ -12,17 +14,25 @@ const AboutModal = () => (
     <Modal trigger={<Menu.Item>About</Menu.Item>}>
       <Modal.Header>About FetchDoggos</Modal.Header>
       <Modal.Content image>
-        <Image wrapped size="medium" src="/images/avatar/large/rachel.png" />
+        <Image wrapped size="medium" src={Remy} />
         <Modal.Description>
           <Header>Description</Header>
           <p>
-            While looking for a fun project to work on with some user interactivity, I was wondering
-            if there was an API for dogs. Much to my surprise I found the
+            One day at the dog park playing with Remy, my Boston Terrier, I wondered if there was an
+            API for dogs. To my surprise there was the
             <StyledLink href="https://dog.ceo" target="_blank" rel="noopener noreferrer">
               DOG API
             </StyledLink>
+            that would allow users to search for random dog images.
           </p>
-          <p>FetchDoggos is an application that uses the power of </p>
+          <p>
+            The goal of this project was to create a project working more with forms and experiement
+            with a new framework besides Material UI.
+          </p>
+          <p>
+            FetchDoggos is built using React, Redux, Semantic UI React, Styled Components, GitHub,
+            and Netlify.
+          </p>
         </Modal.Description>
       </Modal.Content>
     </Modal>
