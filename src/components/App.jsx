45e4051e4c32c15f1fe53a7c3de422b3339ff46b
wfuller <wfuller87@gmail.com>
2019-07-26 14:15:40 -0700

    Sticky footer implementation

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,5 @@
 import React, { Fragment } from 'react';
+import styled from 'styled-components';
 
 import { Container, Divider } from 'semantic-ui-react';
 
@@ -7,15 +8,19 @@ import SearchForm from './SearchForm';
 import DogListContainer from './DogListContainer';
 import Footer from './Footer';
 
+const StyledContainer = styled(Container)`
+  min-height: 100vh;
+`;
+
 const App = () => {
   return (
     <Fragment>
       <Navbar />
-      <Container>
+      <StyledContainer>
         <SearchForm />
         <Divider />
         <DogListContainer />
-      </Container>
+      </StyledContainer>
       <Footer />
     </Fragment>
   );
