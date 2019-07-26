ff7994e6b41286fc8cd989e77e3ddebf86f94647
wfuller <wfuller87@gmail.com>
2019-07-26 13:02:18 -0700

    Fixing mobile navigation links

--- src/components/RandomSearchButton.jsx
+++ src/components/RandomSearchButton.jsx
@@ -2,20 +2,24 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
+import styled from 'styled-components';
 
 import { Button } from 'semantic-ui-react';
 
 import { fetchRandomDoggos } from '../store/actions/doggoActions';
 
-const RandomSearchButton = ({ FETCH_RANDOM_DOGGOS }) => {
-  return (
-    <div>
-      <Button primary onClick={() => FETCH_RANDOM_DOGGOS(3)}>
-        Fetch Random Doggo(s)
-      </Button>
-    </div>
-  );
-};
+const StyledButton = styled(Button)`
+  font-size: 75% !important;
+  @media screen and (min-width: 330px) {
+    font-size: 100% !important;
+  }
+`;
+
+const RandomSearchButton = ({ FETCH_RANDOM_DOGGOS }) => (
+  <StyledButton primary onClick={() => FETCH_RANDOM_DOGGOS(3)}>
+    Fetch Random Doggo(s)
+  </StyledButton>
+);
 
 RandomSearchButton.propTypes = {
   FETCH_RANDOM_DOGGOS: PropTypes.func.isRequired
